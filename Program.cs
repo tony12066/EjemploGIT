c3af96e4b6a807a1df1bdce542ffc61d0a00375c
Anthony Arribasplata <anthony@upn.edu.pe>
2025-08-28 11:02:55 -0500

    Version 2.0

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola estudiantes de cuarto ciclo");
+int n;
+Console.Write("Ingrese un numero: ");
+n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("El numero ingresado fue: " + n);
